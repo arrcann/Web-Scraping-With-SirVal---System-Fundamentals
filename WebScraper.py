7bc60add13208b6ee85da18b0d90cfefe3e35bd1
arrcann <arcalibi011@gmail.com>
2026-05-21 17:39:21 +0800

    Update WebScraper.py

--- WebScraper.py
+++ WebScraper.py
@@ -5,8 +5,8 @@ import json
 import browser_cookie3
 from datetime import datetime, timedelta
 
-ACCOUNT_ID = "COPY PASTE YOUR ACCOUNT ID HERE"
-DEVICE_ID = "COPY PASTE YOUR DEVICE ID HERE"
+ACCOUNT_ID = "ACC-2735603-74738-20"
+DEVICE_ID = "AST-2293597-46342-54"
 
 url = f"https://starlink.com/api/telemetryagg/v1/data-usage/account/{ACCOUNT_ID}/service-line/{DEVICE_ID}/annotated"
 
@@ -14,13 +14,13 @@ headers = {
     "accept": "*/*",
     "accept-language": "en-US",
     "content-type": "application/json",
-    "referer": "COPY PASTE YOUR SUBSCRIPTION URL HERE",
+    "referer": "https://starlink.com/account/service-line/AST-2293597-46342-54?selectedDevice=ut01000000-00000000-0060d786&page=0&limit=5",
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36 OPR/131.0.0.0",
 }
 
 # Automatically grabs fresh cookies from your Opera browser
-cookies = browser_cookie3.Chrome(
-    cookie_file=r"C:\Users\User\AppData\Local\Google\Chrome\User Data\Default\Network\Cookies",
+cookies = browser_cookie3.opera_gx(
+    cookie_file=r"C:\Users\Arrcann\AppData\Roaming\Opera Software\Opera GX Stable\Default\Network\Cookies",
     domain_name="starlink.com"
 )
 
